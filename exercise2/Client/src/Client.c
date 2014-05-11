5ccc81b58229ab7677240a03f4242dafa7c5601c
polyluchs <polyluchs@gmx.net>
2014-05-11 18:08:17 +0200

    - fixed bug that occured when client sends value -1 - removed non-UTF-8 characters from generated source files

--- exercise2/Client/src/Client.c
+++ exercise2/Client/src/Client.c
@@ -162,11 +162,11 @@ int main(int argc, char *argv[]) {
 	}
 	// pdu_resp now contains swapped random string, we can send it back to server
 	enc = der_encode_to_buffer(&asn_DEF_PDU, pdu_resp, buffer, sizeof(buffer));
-
-	if ((write(sock, buffer, strlen(buffer))) == -1) {
+	ssize_t written = 0;
+	if ((written = write(sock, buffer, sizeof(buffer))) == -1) {
 		error_exit("Failed to send value to server.");
 	}
-	printf("sent : %li, %s ", pdu_resp->choice.response.value,
+	printf("sent %i: %li, %s ", written , pdu_resp->choice.response.value,
 			pdu_resp->choice.response.randStr.buf);
 	switch (printOption) {
 	case ENC:
